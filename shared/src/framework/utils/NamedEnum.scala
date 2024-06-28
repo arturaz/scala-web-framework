c389cff18559723099d02f764cb31472b39a8b08
Artūras Šlajus <as@arturaz.net>
2024-06-28 17:12:54 +0300

    Extra `NamedEnum` features.

--- shared/src/framework/utils/NamedEnum.scala
+++ shared/src/framework/utils/NamedEnum.scala
@@ -1,14 +1,32 @@
 package framework.utils
 
 import cats.data.NonEmptyVector
+import cats.syntax.show.*
+import sttp.tapir.Schema
 
 /** Indicates that the enum has conversions from/to [[String]]. Similar to `enumeratum` library, but less boilerplatey
   * and plays nice with Scala 3 `enum`s.
   */
 trait NamedEnum[A] {
-  def toName(a: A): String
-  def fromName(name: String): Option[A]
 
   /** All values of the enum. */
   def values: NonEmptyVector[A]
+
+  /** Turns the value into a name. */
+  def toName(a: A): String
+
+  /** Looks up the value by name. */
+  def fromName(name: String): Option[A] =
+    byName.get(name)
+
+  /** Lookup by name. */
+  val byName: Map[String, A] =
+    values.iterator.map(v => toName(v) -> v).toMap
+
+  def circeKeyCodec: CirceKeyCodec[A] = CirceKeyCodec.instance(fromName)(toName)
+
+  def schema: Schema[A] = Schema
+    .string[A]
+    .encodedExample(show""""${toName(values.head)}"""")
+    .format(show"enum (${values.iterator.map(toName).mkString(" | ")})")
 }
