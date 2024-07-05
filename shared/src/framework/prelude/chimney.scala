3c2d51fa8fd0b6d16193521de6ee5a655be61fcd
Artūras Šlajus <as@arturaz.net>
2024-07-05 18:29:59 +0300

    `intToLongTransformer` instance

--- shared/src/framework/prelude/chimney.scala
+++ shared/src/framework/prelude/chimney.scala
@@ -4,3 +4,5 @@ import io.scalaland.chimney.Transformer
 
 /** An identity transformer. */
 given identityTransformer[A]: Transformer[A, A] = a => a
+
+given intToLongTransformer: Transformer[Int, Long] = _.toLong
