a03cb8f4f6c4a0a1c4990de4554e437db78c3d11
Artūras Šlajus <as@arturaz.net>
2024-07-09 15:02:49 +0300

    Chimney instances.

--- shared/src/framework/prelude/chimney.scala
+++ shared/src/framework/prelude/chimney.scala
@@ -1,8 +1,22 @@
 package framework.prelude
 
 import io.scalaland.chimney.Transformer
+import cats.kernel.Order
 
 /** An identity transformer. */
 given identityTransformer[A]: Transformer[A, A] = a => a
 
 given intToLongTransformer: Transformer[Int, Long] = _.toLong
+
+given nonEmptyVectorTransformer[From, To](using
+  t: Transformer[From, To]
+): Transformer[NonEmptyVector[From], NonEmptyVector[To]] =
+  _.map(t.transform)
+
+given nonEmptyListTransformer[From, To](using
+  t: Transformer[From, To]
+): Transformer[NonEmptyList[From], NonEmptyList[To]] =
+  _.map(t.transform)
+
+given nonEmptySetTransformer[A, B: Order](using t: Transformer[A, B]): Transformer[NonEmptySet[A], NonEmptySet[B]] =
+  _.map(t.transform)
