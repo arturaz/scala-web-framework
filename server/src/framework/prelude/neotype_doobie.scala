0797ac23a7fe9aaf0d4ba26e24de03e8082a0333
Artūras Šlajus <as@arturaz.net>
2024-07-19 17:49:33 +0300

    doobie newtypes should provide Get and Put instead of Read and Write which are for multi-column values

--- server/src/framework/prelude/neotype_doobie.scala
+++ server/src/framework/prelude/neotype_doobie.scala
@@ -1,20 +1,20 @@
 package framework.prelude
 
-import doobie.util.{Read, Write}
+import doobie.util.{Get, Put}
 import doobie.postgres.implicits.*
 import neotype.Newtype
 import framework.utils.NamedEnum
 import org.tpolecat.typename.TypeName
 import doobie.util.meta.Meta
 
-def doobieReadForNewtype[TUnderlying, TWrapperCompanion <: Newtype[TUnderlying]](wrapper: TWrapperCompanion)(using
-  read: Read[TUnderlying]
-): Read[wrapper.Type] =
+def doobieGetForNewtype[TUnderlying, TWrapperCompanion <: Newtype[TUnderlying]](wrapper: TWrapperCompanion)(using
+  read: Get[TUnderlying]
+): Get[wrapper.Type] =
   read.map(wrapper.make(_).getOrThrow)
 
-def doobieWriteForNewtype[TUnderlying, TWrapperCompanion <: Newtype[TUnderlying]](wrapper: TWrapperCompanion)(using
-  write: Write[TUnderlying]
-): Write[wrapper.Type] =
+def doobiePutForNewtype[TUnderlying, TWrapperCompanion <: Newtype[TUnderlying]](wrapper: TWrapperCompanion)(using
+  write: Put[TUnderlying]
+): Put[wrapper.Type] =
   write.contramap(wrapper.unwrap)
 
 /** Constructs a [[Meta]] instance for a PostgreSQL enum. */
