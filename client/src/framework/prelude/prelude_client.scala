6656160a5219796e6504eab06d61f13cd1032e90
Artūras Šlajus <as@arturaz.net>
2024-07-21 23:10:51 +0300

    Send the credentials with CORS requests from client.

--- client/src/framework/prelude/prelude_client.scala
+++ client/src/framework/prelude/prelude_client.scala
@@ -14,12 +14,24 @@ import sttp.model.Uri
 import sttp.tapir.client.sttp.SttpClientInterpreter
 
 import scala.concurrent.{ExecutionContext, Future}
+import sttp.client3.FetchOptions
+
+import org.scalajs.dom.RequestCredentials
 
 given executionContext: ExecutionContext = scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 given ioRuntime: IORuntime = IORuntime.global
 
 given sttpClientInterpreter: SttpClientInterpreter = SttpClientInterpreter()
-given sttpBackend: SttpBackend[IO, WebSockets] = FetchCatsBackend[IO]()
+
+given sttpBackend: SttpBackend[IO, WebSockets] =
+  FetchCatsBackend[IO](
+    FetchOptions.Default.copy(
+      // Include the cookies from the server in the request
+      // https://stackoverflow.com/questions/34558264/fetch-api-with-cookie
+      credentials = Some(RequestCredentials.include)
+    )
+  )
+
 given uriShow: Show[Uri] = _.toString
 
 given ioToFutureFunctionK: (IO ~> Future) = new FunctionK[IO, Future] {
