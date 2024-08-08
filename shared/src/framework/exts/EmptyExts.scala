fb26c1a6bfb63d0f9e8038e6362802eb78318873
Artūras Šlajus <as@arturaz.net>
2024-08-08 13:19:57 +0300

    `.nonEmptyOption`

--- shared/src/framework/exts/EmptyExts.scala
+++ shared/src/framework/exts/EmptyExts.scala
@@ -1,6 +1,7 @@
 package framework.exts
 
 import alleycats.Empty
+import framework.prelude.CanEqual1
 
 /** Allows using `empty[Type]` instead of `Empty[Type].empty`. */
 def empty[A](using empty: Empty[A]): A = empty.empty
@@ -11,8 +12,11 @@ def emptyValue[A](using empty: Empty[A]): A = empty.empty
 extension [A](a: A) {
 
   /** Returns whether the value is the same as the [[Empty]] value. */
-  def isEmpty(using e: Empty[A], eq: CanEqual[A, A]): Boolean = a == e.empty
+  def isEmpty(using e: Empty[A], eq: CanEqual1[A]): Boolean = a == e.empty
 
   /** Returns whether the value is not the same as the [[Empty]] value. */
-  def isNonEmpty(using e: Empty[A], eq: CanEqual[A, A]): Boolean = a != e.empty
+  def isNonEmpty(using e: Empty[A], eq: CanEqual1[A]): Boolean = a != e.empty
+
+  /** Returns [[Some]] if the value is not the same as the [[Empty]] value. */
+  def nonEmptyOption(using e: Empty[A], eq: CanEqual1[A]): Option[A] = if (a == e.empty) None else Some(a)
 }
