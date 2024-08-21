2788a8f0bdad41503f38f21283d5978cc0ba7b6a
Artūras Šlajus <x11@arturaz.net>
2024-08-21 09:25:40 +0300

    Transformers from/to String for sttp Uri

--- shared/src/framework/prelude/sttp.scala
+++ shared/src/framework/prelude/sttp.scala
@@ -1,5 +1,11 @@
 package framework.prelude
 
 import sttp.model.Uri
+import io.scalaland.chimney.PartialTransformer
+import io.scalaland.chimney.partial.Result
+import io.scalaland.chimney.Transformer
 
 given Show[Uri] = _.toString
+
+given PartialTransformer[String, Uri] = PartialTransformer(str => Result.fromEitherString(Uri.parse(str)))
+given Transformer[Uri, String] = _.toString
