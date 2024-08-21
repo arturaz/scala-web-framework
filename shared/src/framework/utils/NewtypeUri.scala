5ddb126ee85ccc158b213869287718c0ef4441cb
Artūras Šlajus <x11@arturaz.net>
2024-08-21 09:27:02 +0300

    `NewtypeUri.unvalidatedTransformer`

--- shared/src/framework/utils/NewtypeUri.scala
+++ shared/src/framework/utils/NewtypeUri.scala
@@ -3,8 +3,16 @@ package framework.utils
 import neotype.Newtype
 import sttp.model.Uri
 import framework.prelude.*
+import framework.exts.*
+import io.scalaland.chimney.Transformer
 
 trait NewtypeUri extends Newtype[Uri] {
   given Conversion[Type, Uri] = unwrap
   given Show[Type] = unwrap(_).toString
+}
+object NewtypeUri {
+
+  /** Makes a [[Transformer]] that assumes you can always make a newtype from an [[Uri]]. */
+  def unvalidatedTransformer(newtype: NewtypeUri): Transformer[Uri, newtype.Type] =
+    uri => newtype.make(uri).getOrThrow
 }
