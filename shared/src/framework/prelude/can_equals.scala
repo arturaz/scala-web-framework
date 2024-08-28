f778d55f9c64af6d64cecefaef9baf10cc36ac86
Artūras Šlajus <as@arturaz.net>
2024-08-28 22:53:05 +0300

    `CanEqual` instance for `Comparison`

--- shared/src/framework/prelude/can_equals.scala
+++ shared/src/framework/prelude/can_equals.scala
@@ -2,6 +2,8 @@ package framework.prelude
 
 import scala.concurrent.duration.Duration
 import java.util.concurrent.TimeUnit
+import cats.kernel.Comparison
 
 given CanEqual[Duration, Duration] = CanEqual.derived
 given CanEqual[TimeUnit, TimeUnit] = CanEqual.derived
+given CanEqual[Comparison, Comparison] = CanEqual.derived
