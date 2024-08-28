5dbabac18f2c43611c8ec0f5bdf48ab3e4c02e44
Artūras Šlajus <as@arturaz.net>
2024-08-28 22:53:38 +0300

    `Schema.schemaForSortedMap` and `.schemaForNonEmptyMap`

--- shared/src/framework/exts/SchemaExts.scala
+++ shared/src/framework/exts/SchemaExts.scala
@@ -1,6 +1,10 @@
 package framework.exts
 
 import sttp.tapir.Schema
+import scala.collection.immutable.SortedMap
+import framework.prelude.CirceKeyEncoder
+import cats.kernel.Order
+import cats.data.NonEmptyMap
 
 extension [A](schema: Schema[A]) {
 
@@ -9,4 +13,17 @@ extension [A](schema: Schema[A]) {
     val newDescription = schema.description.fold(ifEmpty)(ifSome)
     schema.description(newDescription)
   }
+}
+
+extension (obj: Schema.type) {
+  inline def schemaForSortedMap[K: Ordering, V: Schema](using encoder: CirceKeyEncoder[K]): Schema[SortedMap[K, V]] =
+    Schema.schemaForMap[K, V](encoder.apply).map(map => Some(SortedMap.from(map)))(_.toMap)
+
+  inline def schemaForNonEmptyMap[K, V: Schema](using
+    encoder: CirceKeyEncoder[K],
+    ord: Order[K],
+  ): Schema[NonEmptyMap[K, V]] =
+    Schema
+      .schemaForMap[K, V](encoder.apply)
+      .map(map => NonEmptyMap.fromMap(SortedMap.from(map)(using ord.toOrdering)))(_.toSortedMap.toMap)
 }
