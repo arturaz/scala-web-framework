0fd6d04d78a3e27ed667761eb2b374a1acbe703c
Artūras Šlajus <as@arturaz.net>
2024-11-24 16:19:31 +0200

    `Show` for `DefinedAt`

--- shared/src/framework/sourcecode/DefinedAt.scala
+++ shared/src/framework/sourcecode/DefinedAt.scala
@@ -2,12 +2,15 @@ package framework.sourcecode
 
 import cats.syntax.show.*
 import sourcecode.{File, FullName, Line}
+import cats.Show
 
 /** Full definition of a source code position. */
 case class DefinedAt(file: File, line: Line, name: FullName) {
-  override def toString = s"[${name.value.show} @ ${file.value.show}:${line.value.show}]"
+  override def toString = show"[${name.value} @ ${file.value}:${line.value}]"
 }
 object DefinedAt {
   given generate(using file: File, line: Line, name: FullName): DefinedAt =
     apply(file, line, name)
+
+  given show: Show[DefinedAt] = _.toString
 }
