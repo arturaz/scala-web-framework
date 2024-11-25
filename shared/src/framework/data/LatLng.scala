abdfd7f77880150cbd90d5fb8f8a4c5d69139e78
Artūras Šlajus <as@arturaz.net>
2024-11-25 16:10:29 +0200

    `LatLng.zero`

--- shared/src/framework/data/LatLng.scala
+++ shared/src/framework/data/LatLng.scala
@@ -10,3 +10,6 @@ type Longitude = Longitude.Type
 
 /** Latitude and longitude. */
 case class LatLng(latitude: Latitude, longitude: Longitude)
+object LatLng {
+  def zero: LatLng = apply(Latitude(0), Longitude(0))
+}
