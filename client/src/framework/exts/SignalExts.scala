e45d16c0bdc641a369984fa27988d369213e4aba
Artūras Šlajus <as@arturaz.net>
2024-11-26 15:11:04 +0200

    `Signal.splitOptionOrEmpty`

--- client/src/framework/exts/SignalExts.scala
+++ client/src/framework/exts/SignalExts.scala
@@ -5,6 +5,7 @@ import com.raquo.airstream.core.{Observer, Signal}
 import com.raquo.airstream.ownership.ManualOwner
 
 import scala.deriving.Mirror
+import alleycats.Empty
 
 extension [A](signal: Signal[A]) {
 
@@ -22,4 +23,14 @@ extension [A](signal: Signal[A]) {
       }
     }
   }
+
+  /** Maps the value of the [[Signal]] passing the [[A]] as context. */
+  def mapImplicit[B](f: A ?=> B): Signal[B] = signal.map(a => f(using a))
+}
+
+extension [A](signal: Signal[Option[A]]) {
+
+  /** Splits the [[Signal]] by the [[Option]] cases, returning the [[Empty]] value if the [[Option]] is [[None]]. */
+  def splitOptionOrEmpty[B](f: (A, Signal[A]) => B)(using empty: Empty[B]): Signal[B] =
+    signal.splitOption(f, empty.empty)
 }
