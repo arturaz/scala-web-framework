101a6d5f8de13252256f2605fa36d2d0d19bc9f1
Artūras Šlajus <as@arturaz.net>
2024-12-01 21:17:14 +0200

    `applyLoggingDefaults`

--- server/src/framework/prelude/scribe_logging.scala
+++ server/src/framework/prelude/scribe_logging.scala
@@ -1,8 +1,42 @@
 package framework.prelude
 
+import framework.config.IsProductionMode
+import scribe.*
 import scribe.cats.*
-import scribe.{Logger, Scribe}
+import scribe.file.{FileWriter, FlushMode, PathBuilder}
 
-val logGlobal: Scribe[IO] = scribe.cats[IO]
-val appLogger: Logger = scribe.Logger("app")
-val log: Scribe[IO] = appLogger.f[IO]
+import java.nio.charset.StandardCharsets
+import java.nio.file.Paths
+
+// Must be a `def` to make it get the latest applied logger each time.
+def logGlobal: Scribe[IO] = scribe.cats[IO]
+
+// Must be a `def` to make it get the latest applied logger each time.
+def appLogger: Logger = Logger("app")
+
+// Must be a `def` to make it get the latest applied logger each time.
+def log: Scribe[IO] = appLogger.f[IO]
+
+def applyLoggingDefaults(isProduction: IsProductionMode): SyncIO[Unit] = SyncIO {
+  if (!isProduction) {
+    appLogger.info("Applying development logging defaults.")
+
+    // Also log to a file
+    val logPath = Paths.get("development.log")
+    appLogger.info(s"Additionally logging to \"$logPath\".")
+
+    val _ = Logger.root
+      .withHandler(
+        writer = FileWriter(PathBuilder.static(logPath), append = false, FlushMode.AlwaysFlush, StandardCharsets.UTF_8)
+      )
+      // .withMinimumLevel(Level.Debug)
+      .replace()
+
+    val _ = Logger("doobie").withMinimumLevel(Level.Debug).replace()
+
+    // Log at debug level for application code.
+    val _ = appLogger.withMinimumLevel(Level.Debug).replace()
+
+    appLogger.info("Development logging defaults applied.")
+  }
+}
