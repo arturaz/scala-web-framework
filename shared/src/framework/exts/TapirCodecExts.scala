8537729dc071cfbc63a26ddb657aeb1b276108f9
Artūras Šlajus <as@arturaz.net>
2024-12-02 17:31:13 +0200

    `tapir.DecodeResult.toEither`

--- shared/src/framework/exts/TapirCodecExts.scala
+++ shared/src/framework/exts/TapirCodecExts.scala
@@ -1,10 +1,18 @@
 package framework.exts
 
 import framework.prelude.*
+import sttp.tapir.DecodeResult
 
 extension (obj: TapirCodec.type) {
 
   /** Convenience method to create a JSON codec. */
   def fromJsonCodec[A: CirceEncoder: CirceDecoder: Schema]: TapirCodec[String, A, TapirCodecFormat.Json] =
     sttp.tapir.json.circe.circeCodec
+}
+
+extension [A](dr: DecodeResult[A]) {
+  def toEither: Either[DecodeResult.Failure, A] = dr match {
+    case DecodeResult.Value(value)     => Right(value)
+    case failure: DecodeResult.Failure => Left(failure)
+  }
 }
