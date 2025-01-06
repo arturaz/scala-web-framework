e2d849747f619e7f600c4c2d8d8fd7b28ff6ef16
Artūras Šlajus <as@arturaz.net>
2025-01-06 12:05:54 +0200

    CanEqual instances

--- shared/src/framework/prelude/can_equals.scala
+++ shared/src/framework/prelude/can_equals.scala
@@ -1,13 +1,17 @@
 package framework.prelude
 
-import scala.concurrent.duration.Duration
-import java.util.concurrent.TimeUnit
-import cats.kernel.Comparison
-import cats.effect.kernel.Resource.ExitCase
 import cats.effect.ExitCode
+import cats.effect.kernel.Resource.ExitCase
+import cats.kernel.Comparison
+
+import java.time.{LocalDate, LocalDateTime}
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.Duration
 
 given CanEqual1[Duration] = CanEqual.derived
 given CanEqual1[TimeUnit] = CanEqual.derived
 given CanEqual1[Comparison] = CanEqual.derived
+given CanEqual1[LocalDate] = CanEqual.derived
+given CanEqual1[LocalDateTime] = CanEqual.derived
 given CanEqual1[ExitCode] = CanEqual.derived
 given CanEqual1[ExitCase] = CanEqual.derived
