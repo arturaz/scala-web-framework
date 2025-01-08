364f3c82d51307b9e787444d3e931f085d39cec1
Artūras Šlajus <as@arturaz.net>
2025-01-08 12:08:45 +0200

    `F[A].flattenT`, where `F[_] : FlatMap`

--- shared/src/framework/exts/FlatMapExts.scala
+++ shared/src/framework/exts/FlatMapExts.scala
@@ -10,6 +10,26 @@ extension [F[_], A](fa: F[A]) {
   def flatMapT[L, B](f: A => EitherT[F, L, B])(using FlatMap[F]): EitherT[F, L, B] =
     EitherT(fa.flatMap(f(_).value))
 
+  /** Performs a `flatten` lifting into [[EitherT]].
+    *
+    * Useful to have nice expressions when you have an, for example, `IO` and then it becomes `EitherT[IO, Err, A]` at
+    * the end.
+    *
+    * Example:
+    * {{{
+    * (for {
+    *   auth <- authSignal.toIO.map(_.client.authData)
+    *   _ <- IO(lastClickedButton.set(submission.map(_.response)))
+    *   reqId <- reqIdIO
+    * } yield Endpoints.DrugRequests.submitResponse
+    *   .toReq(auth, SubmitPharmacyDrugRequestResponseRequest(reqId, submission))
+    *   .io // EitherT[IO, NetworkError, SubmitPharmacyDrugRequestResponseResponse]
+    * ).flattenT
+    * }}}
+    */
+  def flattenT[L, B](using FlatMap[F], A =:= EitherT[F, L, B]): EitherT[F, L, B] =
+    EitherT(fa.flatMap(_.value))
+
   /** Runs `fa` just for the side-effect of it and then runs `that`. */
   infix def *>[A1, B](that: EitherT[F, A1, B])(using Monad[F]): EitherT[F, A1, B] =
     EitherT.right(fa).flatMap(_ => that)
