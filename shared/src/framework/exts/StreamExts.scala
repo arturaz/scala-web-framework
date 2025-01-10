bdbfdda1cec224661f85e5207d4406bea17d8c26
Artūras Šlajus <as@arturaz.net>
2025-01-10 17:39:08 +0200

    `Stream.mapFilter`

--- shared/src/framework/exts/StreamExts.scala
+++ shared/src/framework/exts/StreamExts.scala
@@ -7,4 +7,12 @@ extension [F[_], A](stream: fs2.Stream[F, A]) {
   /** Evaluates the given function on each chunk of the stream. */
   def evalTapChunks[X](f: Chunk[A] => F[X]): fs2.Stream[F, A] =
     stream.chunks.evalTap(f).unchunks
+
+  /** Maps each chunk of the stream using the given function, only keeping the [[Some]] results. */
+  def mapFilter[B](f: A => Option[B]): fs2.Stream[F, B] =
+    stream.mapChunks(chunk => chunk.mapFilter(f))
+
+  /** Alias for [[mapFilter]]. */
+  inline def collectSome[B](f: A => Option[B]): fs2.Stream[F, B] =
+    mapFilter(f)
 }
