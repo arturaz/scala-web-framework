da226fb70c86ccb82d5f8ec73e2d506e311e4f6c
Artūras Šlajus <as@arturaz.net>
2025-01-10 17:18:41 +0200

    `Signal.toEventStream`

--- client/src/framework/exts/SignalExts.scala
+++ client/src/framework/exts/SignalExts.scala
@@ -5,6 +5,7 @@ import com.raquo.airstream.core.{Observer, Signal}
 import com.raquo.airstream.ownership.ManualOwner
 
 import alleycats.Empty
+import com.raquo.airstream.misc.StreamFromSignal
 
 extension [A](signal: Signal[A]) {
 
@@ -25,6 +26,9 @@ extension [A](signal: Signal[A]) {
 
   /** Maps the value of the [[Signal]] passing the [[A]] as context. */
   def mapImplicit[B](f: A ?=> B): Signal[B] = signal.map(a => f(using a))
+
+  // This one is a bit iffy, but hey, we have no better option... https://github.com/raquo/Airstream/issues/124
+  def toEventStream: EventStream[A] = new StreamFromSignal[A](parent = signal, changesOnly = false)
 }
 
 extension [A](signal: Signal[Option[A]]) {
