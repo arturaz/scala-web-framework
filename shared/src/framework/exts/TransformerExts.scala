89a704222312938e485015ce75d310e1429a3057
Artūras Šlajus <as@arturaz.net>
2025-01-14 17:41:29 +0200

    `Transformer.map` and `.contramap`

--- shared/src/framework/exts/TransformerExts.scala
+++ shared/src/framework/exts/TransformerExts.scala
@@ -11,6 +11,12 @@ implicit class TransformerExts[From, To](t: Transformer[From, To]) extends AnyVa
   def andThen[To2](t2: Transformer[To, To2]): Transformer[From, To2] =
     from => t2.transform(t.transform(from))
 
+  def map[To2](f: To => To2): Transformer[From, To2] =
+    from => f(t.transform(from))
+
+  def contramap[From2](f: From2 => From): Transformer[From2, To] =
+    from => t.transform(f(from))
+
   def asPartialTransformer: PartialTransformer[From, To] =
     PartialTransformer.fromFunction(t.transform)
 }
