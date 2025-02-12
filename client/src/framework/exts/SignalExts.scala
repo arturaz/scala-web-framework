a4e45cd680d03c68fb53f30984de5052cd357ca1
Artūras Šlajus <as@arturaz.net>
2025-02-12 13:02:22 +0200

    `toIOMapFilter` and friends

--- client/src/framework/exts/SignalExts.scala
+++ client/src/framework/exts/SignalExts.scala
@@ -9,13 +9,22 @@ import com.raquo.airstream.ownership.ManualOwner
 extension [A](signal: Signal[A]) {
 
   /** Subscribes to a [[Signal]] to receive the first value and immediately unsubscribes. */
-  def toIO: IO[A] = {
-    IO.async[A] { callback =>
+  def toIO: IO[A] =
+    signal.toIOMapFilter(Some(_))
+
+  /** Subscribes to a [[Signal]] to receive the first value that matches the predicate and immediately unsubscribes. */
+  def toIOMapFilter[B](f: A => Option[B]): IO[B] = {
+    IO.async[B] { callback =>
       IO {
         val owner = new ManualOwner
         signal.addObserver(Observer(a => {
-          owner.killSubscriptions()
-          callback(Right(a))
+          f(a) match {
+            case None =>
+            // do nothing
+            case Some(b) =>
+              owner.killSubscriptions()
+              callback(Right(b))
+          }
         }))(owner)
 
         Some(IO(owner.killSubscriptions()))
@@ -30,7 +39,8 @@ extension [A](signal: Signal[A]) {
   // https://github.com/raquo/Airstream/issues/124
   // https://github.com/raquo/Airstream/issues/132
   // https://discordapp.com/channels/1020225759610163220/1020225760075718669/1327296294959448229
-  def toEventStream: EventStream[A] = EventStream.merge(EventStream.unit().sample(signal), signal.changes)
+  def toEventStream: EventStream[A] =
+    EventStream.merge(EventStream.unit().sample(signal), signal.changes)
 
   /** Gives a window of the previous and the current value of the [[Signal]]. */
   def changedValues: Signal[(Option[A], A)] = {
@@ -39,6 +49,14 @@ extension [A](signal: Signal[A]) {
 }
 
 extension [A](signal: Signal[Option[A]]) {
+
+  /** Subscribes to a [[Signal]] to receive the first `Some` value and immediately unsubscribes. */
+  def toIOOnSome: IO[A] =
+    signal.toIOMapFilter(identity)
+
+  /** Subscribes to a [[Signal]] to receive the first `None` value and immediately unsubscribes. */
+  def toIOOnNone: IO[Unit] =
+    signal.toIOMapFilter { case None => Some(()); case Some(_) => None }
 
   /** Splits the [[Signal]] by the [[Option]] cases, returning the [[Empty]] value if the [[Option]] is [[None]]. */
   def splitOptionOrEmpty[B](f: (A, Signal[A]) => B)(using empty: Empty[B]): Signal[B] =
