0c55a585f11bee020a286c8d088016322b1c46dd
Artūras Šlajus <as@arturaz.net>
2025-02-12 22:43:38 +0200

    `RequestSuccessful.fromOption`

--- shared/src/framework/api/RequestSuccessful.scala
+++ shared/src/framework/api/RequestSuccessful.scala
@@ -7,5 +7,13 @@ import framework.prelude.CanEqual1
 object RequestSuccessful extends NewtypeBoolean {
   def fromExpected[A: CanEqual1](expected: A)(actual: A): RequestSuccessful =
     if (expected == actual) apply(true) else apply(false)
+
+  /** If the `Option` is `Some` then the request was successful, if the `Option` is `None` then the request was not
+    * successful.
+    */
+  def fromOption(option: Option[Unit]): RequestSuccessful = option match {
+    case Some(()) => apply(true)
+    case None     => apply(false)
+  }
 }
 type RequestSuccessful = RequestSuccessful.Type
