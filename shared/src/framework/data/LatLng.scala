d4a4595d15944ebc63be5c6dd8aae2ac74233aa2
Artūras Šlajus <as@arturaz.net>
2025-02-12 22:43:29 +0200

    Instances for location types.

--- shared/src/framework/data/LatLng.scala
+++ shared/src/framework/data/LatLng.scala
@@ -1,7 +1,9 @@
 package framework.data
 
-import yantl.*
 import framework.utils.NewtypeDouble
+import framework.prelude.{*, given}
+import sttp.tapir.{Schema, SchemaType}
+import yantl.*
 
 object Latitude extends NewtypeDouble {
   type TError = ValidatorRule.SmallerThan[Double] | ValidatorRule.LargerThan[Double]
@@ -11,6 +13,7 @@ object Latitude extends NewtypeDouble {
   )
 
   val zero = makeOrThrow(0)
+  given Schema[Type] = Schema(SchemaType.SNumber()).description("Latitude in degrees, between -90 and 90 inclusive.")
 }
 type Latitude = Latitude.Type
 
@@ -22,15 +25,19 @@ object Longitude extends NewtypeDouble {
   )
 
   val zero = makeOrThrow(0)
+  given Schema[Type] = Schema(SchemaType.SNumber()).description("Longitude in degrees, between -180 and 180 inclusive.")
 }
 type Longitude = Longitude.Type
 
 /** Latitude and longitude. */
-case class LatLng(latitude: Latitude, longitude: Longitude)
+case class LatLng(latitude: Latitude, longitude: Longitude) derives CanEqual, Schema, CirceCodec
 object LatLng {
   def zero: LatLng = apply(Latitude.zero, Longitude.zero)
 
   trait Newtype extends Newtype.WithoutValidationOf[LatLng] { self =>
     val zero: Type = self.apply(LatLng.zero)
+
+    given schema: Schema[Type] = summon[Schema[LatLng]].map(v => Some(self.apply(v)))(unwrap)
+    given circeCodec: CirceCodec[Type] = summon[CirceCodec[LatLng]].imap(self.apply)(unwrap)
   }
 }
