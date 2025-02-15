1c1be5f97774a82761a9b8076696b2c8806cd6d7
Artūras Šlajus <as@arturaz.net>
2025-02-15 22:33:02 +0200

    `headOrEmpty`, `lastOrEmpty`

--- shared/src/framework/exts/AnyExts.scala
+++ shared/src/framework/exts/AnyExts.scala
@@ -3,6 +3,7 @@ package framework.exts
 import framework.utils.{MaybeGetHead, MaybeGetLast}
 import scala.collection.Factory
 import scala.reflect.ClassTag
+import alleycats.Empty
 
 extension [A](a: A) {
   def pprint: String = _root_.pprint.apply(a).render
@@ -13,9 +14,17 @@ extension [A](a: A) {
   def headOption[Element](using typeclass: MaybeGetHead[A, Element]): Option[Element] =
     typeclass.headOption(a)
 
+  /** Gets the first element in the collection or returns an empty value. */
+  def headOrEmpty[Element](using MaybeGetHead[A, Element], Empty[Element]): Element =
+    a.headOption.getOrEmpty
+
   /** Gets the last element in the collection. */
   def lastOption[Element](using typeclass: MaybeGetLast[A, Element]): Option[Element] =
     typeclass.lastOption(a)
+
+  /** Gets the last element in the collection or returns an empty value. */
+  def lastOrEmpty[Element](using MaybeGetLast[A, Element], Empty[Element]): Element =
+    a.lastOption.getOrEmpty
 }
 
 extension [A, C[X]](value: A | C[A]) {
