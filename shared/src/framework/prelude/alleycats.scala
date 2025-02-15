4755d8fe750f4dc05cc8b70357ff208fbce37a5c
Artūras Šlajus <as@arturaz.net>
2025-02-15 22:32:50 +0200

    names

--- shared/src/framework/prelude/alleycats.scala
+++ shared/src/framework/prelude/alleycats.scala
@@ -4,6 +4,6 @@ import alleycats.Empty
 import framework.exts.empty
 import cats.data.{NonEmptyList, NonEmptyVector}
 
-given [A: Empty]: Empty[NonEmptyVector[A]] = Empty(NonEmptyVector.one(empty[A]))
-given [A: Empty]: Empty[NonEmptyList[A]] = Empty(NonEmptyList.one(empty[A]))
+given nonEmptyVectorOfEmptyValue[A: Empty]: Empty[NonEmptyVector[A]] = Empty(NonEmptyVector.one(empty[A]))
+given nonEmptyListOfEmptyValue[A: Empty]: Empty[NonEmptyList[A]] = Empty(NonEmptyList.one(empty[A]))
 given emptyOption[A]: Empty[Option[A]] = Empty(None)
