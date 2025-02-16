daec10fec60736093d14bc0870ca0bdbcf13a17f
Artūras Šlajus <as@arturaz.net>
2025-02-17 00:01:12 +0200

    `Schema` for `NewtypeJson`

--- shared/src/framework/utils/NewtypeJson.scala
+++ shared/src/framework/utils/NewtypeJson.scala
@@ -6,10 +6,13 @@ import framework.prelude.*
 import io.circe.Json
 import io.scalaland.chimney.Transformer
 import yantl.Newtype
+import sttp.tapir.SchemaType
 
 /** A newtype wrapping a [[Json]]. */
 trait NewtypeJson extends Newtype.Of[Json] {
   given Show[Type] = unwrap(_).noSpacesSortKeys
   given CanEqual[Type, Type] = CanEqual.derived
   given Transformer[Type, Json] = unwrap
+
+  def schemaFor: Schema[Type] = Schema(SchemaType.SBinary()).description("JSON")
 }
