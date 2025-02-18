be5e04f183372fd0716f91226d1e2e7095339e72
Artūras Šlajus <as@arturaz.net>
2025-02-18 12:34:24 +0200

    `Seq.indexOfOption`, `Seq.findWithIndex`

--- shared/src/framework/exts/SeqExts.scala
+++ shared/src/framework/exts/SeqExts.scala
@@ -4,4 +4,15 @@ extension [A](seq: Seq[A]) {
 
   /** Upcasts the type to [[Seq]] from a specific type. */
   def asSeq: Seq[A] = seq
+
+  /** As [[Seq.indexOf]] but returns an [[Option]]. */
+  def indexOfOption(elem: A, from: Int = 0): Option[Int] =
+    seq.indexOf(elem, from) match {
+      case -1  => None
+      case idx => Some(idx)
+    }
+
+  /** As [[Seq.find]] but returns an [[Option]] of the value and its index. */
+  def findWithIndex(f: A => Boolean): Option[(A, Int)] =
+    seq.iterator.zipWithIndex.find { case (a, _) => f(a) }
 }
