3c015b6b0228701391f7e37889d3f9ef95e024f9
Artūras Šlajus <x11@arturaz.net>
2025-02-28 12:58:04 +0200

    `WithLogger`

--- server/src/framework/prelude/scribe_logging.scala
+++ server/src/framework/prelude/scribe_logging.scala
@@ -17,6 +17,13 @@ def appLogger: Logger = Logger("app")
 // Must be a `def` to make it get the latest applied logger each time.
 def log: Scribe[IO] = appLogger.f[IO]
 
+trait WithLogger {
+  lazy private val classNameForLogging = getClass().getCanonicalName()
+
+  // Must be a `def` to make it get the latest applied logger each time.
+  def log: Scribe[IO] = Logger(classNameForLogging).f[IO]
+}
+
 def applyLoggingDefaults(isProduction: IsProductionMode): SyncIO[Unit] = SyncIO {
   if (!isProduction) {
     appLogger.info("Applying development logging defaults.")
