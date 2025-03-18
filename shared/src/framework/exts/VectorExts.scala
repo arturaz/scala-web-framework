4c72748388beb34a1085888d7d848ab50291ef43
Artūras Šlajus <x11@arturaz.net>
2025-03-18 17:51:27 +0200

    `Vector.replaceAt`

--- shared/src/framework/exts/VectorExts.scala
+++ shared/src/framework/exts/VectorExts.scala
@@ -5,6 +5,12 @@ implicit class VectorExts[A](private val vector: Vector[A]) extends AnyVal {
   /** Removes the element at the given index. */
   def removeAt(idx: Int): Vector[A] = {
     val (before, after) = vector.splitAt(idx)
-    before ++ after.drop(1)
+    before ++ after.tail
+  }
+
+  /** Replaces the element at the given index. */
+  def replaceAt(idx: Int, elem: A): Vector[A] = {
+    val (before, after) = vector.splitAt(idx)
+    (before :+ elem) ++ after.tail
   }
 }
