d74f8573992b4c4b87b3a0cefeddbe6a876cbd7d
Artūras Šlajus <x11@arturaz.net>
2025-03-18 17:51:21 +0200

    `Seq.findMap`, `Seq.indexWhereOption`

--- shared/src/framework/exts/SeqExts.scala
+++ shared/src/framework/exts/SeqExts.scala
@@ -12,7 +12,18 @@ extension [A](seq: Seq[A]) {
       case idx => Some(idx)
     }
 
+  /** As [[Seq.indexWhere]] but returns an [[Option]]. */
+  def indexWhereOption(f: A => Boolean, from: Int = 0): Option[Int] =
+    seq.indexWhere(f, from) match {
+      case -1  => None
+      case idx => Some(idx)
+    }
+
   /** As [[Seq.find]] but returns an [[Option]] of the value and its index. */
   def findWithIndex(f: A => Boolean): Option[(A, Int)] =
     seq.iterator.zipWithIndex.find { case (a, _) => f(a) }
+
+  /** Finds the first element that returns [[Some]] from the given function. */
+  def findMap[B](f: A => Option[B]): Option[B] =
+    seq.iterator.map(f).find(_.isDefined).flatten
 }
