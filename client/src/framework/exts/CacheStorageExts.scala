f6be410434a4b52fe5af9cfef3e1a869d0d0dc60
Artūras Šlajus <x11@arturaz.net>
2025-03-25 23:16:56 +0200

    Better logging in `CacheStorage.deleteAll`

--- client/src/framework/exts/CacheStorageExts.scala
+++ client/src/framework/exts/CacheStorageExts.scala
@@ -8,9 +8,10 @@ import scala.concurrent.Future
 extension (storage: CacheStorage) {
   def deleteAll(predicate: String => Boolean = _ => true, log: JSLogger = JSLogger.noOp): Future[Unit] =
     for {
-      cacheKeys <- storage.keys().toFuture.map(_.iterator.filter(predicate).toVector)
-      _ = log.debug(s"Clearing caches: $cacheKeys")
-      _ <- Future.traverse(cacheKeys) { cache =>
+      cacheKeys <- storage.keys().toFuture.map(_.toVector)
+      clearedCaches = cacheKeys.filter(predicate)
+      _ = log.debug(s"Clearing caches: $clearedCaches, all available caches: $cacheKeys")
+      _ <- Future.traverse(clearedCaches) { cache =>
         log.debug(s"Clearing cache: $cache")
         storage.delete(cache).toFuture
       }
