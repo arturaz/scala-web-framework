38f78dd61a5d9e3f5deb708c2ca08d2f380f0fee
Artūras Šlajus <x11@arturaz.net>
2025-03-25 22:27:45 +0200

    docs

--- client/src/framework/utils/ServiceWorkerRegistration.scala
+++ client/src/framework/utils/ServiceWorkerRegistration.scala
@@ -5,6 +5,11 @@ import org.scalajs.dom.ServiceWorkerRegistration
 import framework.exts.*
 import cats.syntax.all.*
 
+/** Utilities for working with service workers.
+  *
+  * Also see https://gist.github.com/Rich-Harris/fd6c3c73e6e707e312d7c5d7d0f3b2f9 for heap of useful information about
+  * this monstrosity.
+  */
 object ServiceWorkerRegistration {
 
   /** Returns [[None]] if service workers are not supported. */
