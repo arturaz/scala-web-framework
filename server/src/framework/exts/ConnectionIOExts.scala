bfdc352861effe8dbe0f941f6fed5e41ae6bf12a
Artūras Šlajus <x11@arturaz.net>
2025-03-31 22:16:47 +0300

    Extra `ConnectionIO` helpers.

--- server/src/framework/exts/ConnectionIOExts.scala
+++ server/src/framework/exts/ConnectionIOExts.scala
@@ -8,22 +8,34 @@ extension [A](connectionIO: ConnectionIO[A]) {
   /** Alias for `transact` that is less verbose. */
   def perform(using tx: Transactor[IO], tracer: Tracer[IO], definedAt: sourcecode.FullName): IO[A] =
     tracer.span(show"dbIO: ${definedAt.value}").surround(connectionIO.transact(tx))
+
+  /** Raises an exception aborting the transaction if the value does not match. */
+  def throwIfNot(value: A)(using definedAt: DefinedAt)(using CanEqual1[A]): ConnectionIO[Unit] =
+    connectionIO.flatMap {
+      case `value` => ConnectionIO.unit
+      case other   => ConnectionIO.raiseError(new Exception(s"Expected $value, got $other at $definedAt"))
+    }
+
+  /** Returns true if the value matches. */
+  def falseIfNot(value: A)(using CanEqual1[A]): ConnectionIO[Boolean] =
+    connectionIO.map(_ == value)
+
+  /** Returns an [[OptionT]] with `Some` if the value matches, `None` otherwise. */
+  def valueOrNoneT(value: A)(using CanEqual1[A]): OptionT[ConnectionIO, A] =
+    OptionT(connectionIO.map(actual => if (actual == value) Some(actual) else None))
 }
 
 extension (connectionIO: ConnectionIO[Int]) {
 
   /** Raises an exception aborting the transaction if the number of affected rows is not 1. */
   def singleOrThrow_!(using definedAt: DefinedAt): ConnectionIO[Unit] =
-    connectionIO.flatMap {
-      case 1     => ConnectionIO.unit
-      case other => ConnectionIO.raiseError(new Exception(s"Expected 1 row, got $other at $definedAt"))
-    }
+    connectionIO.throwIfNot(1)
 
   /** Returns true if the number of affected rows is 1. */
   def singleOrFalse: ConnectionIO[Boolean] =
-    connectionIO.map(_ == 1)
+    connectionIO.falseIfNot(1)
 
   /** Returns an [[OptionT]] with `Some` if the update was successful, `None` otherwise. */
   def singleOrNoneT: OptionT[ConnectionIO, Unit] =
-    OptionT(connectionIO.map(rows => if (rows == 1) Some(()) else None))
+    connectionIO.valueOrNoneT(1).void
 }
