06a0d6392b509feaf1da4f6869e00e258792634d
Artūras Šlajus <x11@arturaz.net>
2025-04-01 17:33:33 +0300

    Turns out this is not needed, as `.parJoinUnbounded` exists

--- shared/src/framework/exts/StreamExts.scala
+++ shared/src/framework/exts/StreamExts.scala
@@ -1,9 +1,8 @@
 package framework.exts
 
-import fs2.Chunk
-import cats.effect.Concurrent
+import fs2.{Chunk, Stream}
 
-extension [F[_], A](stream: fs2.Stream[F, A]) {
+extension [F[_], A](stream: Stream[F, A]) {
 
   /** Evaluates the given function on each chunk of the stream. */
   def evalTapChunks[X](f: Chunk[A] => F[X]): fs2.Stream[F, A] =
@@ -16,11 +15,4 @@ extension [F[_], A](stream: fs2.Stream[F, A]) {
   /** Alias for [[mapFilter]]. */
   inline def collectSome[B](f: A => Option[B]): fs2.Stream[F, B] =
     mapFilter(f)
-}
-
-extension [F[_], A](iter: IterableOnce[fs2.Stream[F, A]]) {
-
-  /** Merges all streams in the iterator into a single stream. */
-  def mergeAll(using Concurrent[F]): fs2.Stream[F, A] =
-    iter.iterator.foldLeft(fs2.Stream.empty: fs2.Stream[F, A])(_.merge(_))
 }
