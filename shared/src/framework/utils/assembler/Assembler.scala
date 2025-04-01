51f57cf2db0b304de29c5b07c158f8a216232bb2
Artūras Šlajus <x11@arturaz.net>
2025-04-01 14:10:52 +0300

    `Assembler.fromSeparateQueries`

--- shared/src/framework/utils/assembler/Assembler.scala
+++ shared/src/framework/utils/assembler/Assembler.scala
@@ -1,27 +1,47 @@
 package framework.utils.assembler
 
+import framework.exts.*
+
 /** Converts relational data to hierarchical data. */
 object Assembler {
 
-  /** Example:
-    * {{{
-    * val children: Vector[(OutstandingDrugRequestDB, Option[ListDrugRequestsPharmacyResponse])]
+  /** Processes results that you get after an SQL join.
+    *
+    * If you have two tables, DrugRequests and PharmacyResponses and execute a query like:
+    * ```
+    * SELECT reqs.*, responses.* FROM DrugRequests reqs
+    * LEFT JOIN PharmacyResponses responses ON reqs.id = responses.requestId
+    * ```
+    *
+    * Then you can use `Assembler` to convert the results to a hierarchical structure:
+    * ```scala
+    * val results: Vector[(OutstandingDrugRequestDB, Option[ListDrugRequestsPharmacyResponse])]
     *
     * val itemsForGP: Vector[ListDrugRequestsItem] =
-    *   Assembler(
-    *     children,
-    *     { case (req, _) => req.id },
-    *     { case (_, response) => response },
-    *     { case ((req, _), responses) =>
-    *       ListDrugRequestsItem(req.id, req.data, responses.toVector, req.acceptedResponse)
-    *     },
-    *   )
-    *   .toVector
-    * }}}
+    *   Assembler
+    *     .fromJoinResult(
+    *       items = results,
+    *       itemToId = { case (req, _) => req.id },
+    *       itemToChild = { case (_, response) => response },
+    *       itemToResult = { case ((req, _), responses) =>
+    *         ListDrugRequestsItem(req.id, req.data, responses.toVector, req.acceptedResponse)
+    *       },
+    *     )
+    *     .toVector
+    * ```
     *
+    * @param items
+    *   The joined rows.
+    * @param itemToId
+    *   Extracts the ID of an item from the joined row.
+    * @param itemToChild
+    *   Extracts the child (which can be non-existent, thus [[Option]]) of an item from the joined row.
+    * @param itemToResult
+    *   Converts an item and its present children to a result.
     * @return
+    *   An iterator of results.
     */
-  def apply[Item, ItemId, Child, Result](
+  def fromJoinResult[Item, ItemId, Child, Result](
     items: Iterable[Item],
     itemToId: Item => ItemId,
     itemToChild: Item => Option[Child],
@@ -37,5 +57,66 @@ object Assembler {
       }
 
     results
+  }
+
+  /** Processes results from two separate queries.
+    *
+    * For example, you have two tables, DrugRequests and PharmacyResponses, and execute SQL queries:
+    * ```sql
+    * SELECT * FROM DrugRequests reqs
+    * ```
+    *
+    * and
+    *
+    * ```sql
+    * SELECT * FROM PharmacyResponses responses
+    * ```
+    *
+    * Then you can use `Assembler` to convert the results to a hierarchical structure:
+    * ```scala
+    * val requests: Vector[DrugRequests.Row]
+    * val responses: Vector[PharmacyResponses.Row]
+    *
+    * val itemsForGP: Vector[ListDrugRequestsItem] =
+    *   Assembler
+    *     .fromSeparateQueries(
+    *       parentRows = requests,
+    *       childRows = responses,
+    *       parentToId = { req => req.id },
+    *       childToParentId = { response => response.requestId },
+    *       childToResult = { (req, responses) =>
+    *         ListDrugRequestsItem(req.id, req.data, responses.toVector, req.acceptedResponse)
+    *       },
+    *     )
+    *     .toVector
+    * ```
+    *
+    * @param parentRows
+    *   The rows of the parent table.
+    * @param parentToId
+    *   Extracts the ID of a parent from a row.
+    * @param childRows
+    *   The rows of the children table.
+    * @param childToParentId
+    *   Extracts the ID of a child from a row.
+    * @param childToResult
+    *   Converts a parent and its children to a result.
+    * @return
+    *   An iterator of results.
+    */
+  def fromSeparateQueries[ParentRow, ParentId, ChildRow, Result](
+    parentRows: Iterable[ParentRow],
+    parentToId: ParentRow => ParentId,
+    childRows: Iterable[ChildRow],
+    childToParentId: ChildRow => ParentId,
+    childToResult: (ParentRow, Iterator[ChildRow]) => Result,
+  ): Iterator[Result] = {
+    val childrenByParentId = childRows.groupBy(childToParentId)
+
+    parentRows.iterator.map { parentRow =>
+      val parentId = parentToId(parentRow)
+      val children = childrenByParentId.get(parentId).fold2(Iterator.empty, _.iterator)
+      childToResult(parentRow, children)
+    }
   }
 }
--- shared/tests/src/framework/utils/assembler/AssemblerTest.scala
+++ shared/tests/src/framework/utils/assembler/AssemblerTest.scala
@@ -3,7 +3,7 @@ package framework.utils.assembler
 import framework.utils.FrameworkTestSuite
 
 class AssemblerTest extends FrameworkTestSuite {
-  test("grouping") {
+  test("fromJoinResult: grouping") {
     case class Data(id: Int, name: String, address: String)
     val items = Vector(
       Data(1, "John", "123 Main St"),
@@ -15,12 +15,14 @@ class AssemblerTest extends FrameworkTestSuite {
 
     case class DataGrouped(id: Int, name: String, addresses: Vector[String])
 
-    val actual = Assembler(
-      items,
-      _.id,
-      i => Some(i.address),
-      (i, addresses) => DataGrouped(i.id, i.name, addresses.toVector),
-    ).toVector
+    val actual = Assembler
+      .fromJoinResult(
+        items,
+        _.id,
+        i => Some(i.address),
+        (i, addresses) => DataGrouped(i.id, i.name, addresses.toVector),
+      )
+      .toVector
 
     val expected = Vector(
       DataGrouped(1, "John", Vector("123 Main St", "456 Elm St")),
@@ -31,7 +33,7 @@ class AssemblerTest extends FrameworkTestSuite {
     actual shouldBe expected
   }
 
-  test("grouping 2 levels") {
+  test("fromJoinResult: grouping 2 levels") {
     case class Data(houseId: Int, houseName: String, address: String, apartmentNo: Int, tenantName: String)
 
     val items = Vector(
@@ -47,26 +49,67 @@ class AssemblerTest extends FrameworkTestSuite {
     case class Apartment(apartmentNo: Int, tenants: Vector[String])
     case class House(houseId: Int, houseName: String, address: String, apartments: Vector[Apartment])
 
-    val actual = Assembler(
-      items,
-      _.houseId,
-      i => Some((i.apartmentNo, i.tenantName)),
-      (i, apartments) => {
-        val groupedApartments = Assembler(
-          apartments.toArray,
-          _._1,
-          tpl => Some(tpl._2),
-          (tpl, tenants) => Apartment(tpl._1, tenants.toVector),
-        ).toVector
+    val actual = Assembler
+      .fromJoinResult(
+        items,
+        _.houseId,
+        i => Some((i.apartmentNo, i.tenantName)),
+        (i, apartments) => {
+          val groupedApartments = Assembler
+            .fromJoinResult(
+              apartments.toArray,
+              _._1,
+              tpl => Some(tpl._2),
+              (tpl, tenants) => Apartment(tpl._1, tenants.toVector),
+            )
+            .toVector
 
-        House(i.houseId, i.houseName, i.address, groupedApartments)
-      },
-    ).toVector
+          House(i.houseId, i.houseName, i.address, groupedApartments)
+        },
+      )
+      .toVector
 
     val expected = Vector(
       House(1, "House 1", "123 Main St", Vector(Apartment(1, Vector("John", "Jane")), Apartment(2, Vector("Bob")))),
       House(2, "House 2", "456 Elm St", Vector(Apartment(1, Vector("Bob")), Apartment(2, Vector("Alice", "Charlie")))),
       House(3, "House 3", "789 Oak St", Vector(Apartment(1, Vector("Charlie")))),
+    )
+
+    actual shouldBe expected
+  }
+
+  test("fromSeparateQueries: grouping") {
+    case class Data(id: Int, name: String, address: String)
+    case class ChildData(parentId: Int, name: String)
+
+    case class GroupedData(id: Int, name: String, children: Vector[String])
+
+    val datum = Vector(
+      Data(1, "John", "123 Main St"),
+      Data(2, "Jane", "789 Oak St"),
+      Data(3, "Bob", "222 Cedar St"),
+    )
+
+    val childDatum = Vector(
+      ChildData(1, "John"),
+      ChildData(1, "Jane"),
+      ChildData(2, "Bob"),
+    )
+
+    val actual = Assembler
+      .fromSeparateQueries(
+        datum,
+        _.id,
+        childDatum,
+        _.parentId,
+        (d, cs) => GroupedData(d.id, d.name, cs.map(_.name).toVector),
+      )
+      .toVector
+
+    val expected = Vector(
+      GroupedData(1, "John", Vector("John", "Jane")),
+      GroupedData(2, "Jane", Vector("Bob")),
+      GroupedData(3, "Bob", Vector.empty),
     )
 
     actual shouldBe expected
