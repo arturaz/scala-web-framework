dfe653ada632a460b0b9d804b0fd0897b7c16ecf
Artūras Šlajus <x11@arturaz.net>
2025-04-02 17:18:00 +0300

    This is not needed in new alleycats.

--- shared/src/framework/exts/ExtractExts.scala
+++
@@ -1,8 +0,0 @@
-package framework.exts
-
-import alleycats.Extract
-
-/** Needed due to https://github.com/typelevel/cats/issues/4642 */
-extension [F[_], A](fa: F[A]) {
-  def extract(using X: Extract[F]): A = X.extract(fa)
-}
