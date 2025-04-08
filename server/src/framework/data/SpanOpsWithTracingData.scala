da340555fbb70b5af38d785224bccc4bd681cf0d
Artūras Šlajus <x11@arturaz.net>
2025-04-08 18:25:48 +0300

    Functor for SpanOpsWithTracingData

--- server/src/framework/data/SpanOpsWithTracingData.scala
+++ server/src/framework/data/SpanOpsWithTracingData.scala
@@ -1,7 +1,7 @@
 package framework.data
 
-import org.typelevel.otel4s.trace.SpanOps
-import org.typelevel.otel4s.trace.Span
+import cats.Functor
+import org.typelevel.otel4s.trace.{Span, SpanOps}
 
 final case class SpanOpsWithTracingData[F[_], A](ops: SpanOps[F], data: WithTracingData[A]) {
 
@@ -12,4 +12,13 @@ final case class SpanOpsWithTracingData[F[_], A](ops: SpanOps[F], data: WithTrac
 
   /** As [[use]] but does not give you access to the [[Span]]. */
   def useValue[B](f: A => F[B])(using tracer: Tracer[F]): F[B] = use((span, a) => f(a))
+
+  def map[B](f: A => B): SpanOpsWithTracingData[F, B] =
+    SpanOpsWithTracingData(ops, data.map(f))
+}
+object SpanOpsWithTracingData {
+  given functor[F[_]]: Functor[[A] =>> SpanOpsWithTracingData[F, A]] with {
+    override def map[A, B](fa: SpanOpsWithTracingData[F, A])(f: A => B): SpanOpsWithTracingData[F, B] =
+      fa.map(f)
+  }
 }
