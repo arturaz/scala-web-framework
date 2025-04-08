cb97d120e432bc142c18b0f028fe4bea08655b44
Artūras Šlajus <x11@arturaz.net>
2025-04-08 12:30:53 +0300

    CellWithOverlay: make overlay take a Node

--- clientDaisyUI/src/framework/components/CellWithOverlay.scala
+++ clientDaisyUI/src/framework/components/CellWithOverlay.scala
@@ -2,9 +2,14 @@ package framework.components
 
 import L.*
 
+/** The contents of the overlay for [[CellWithOverlay]]. */
+type CellOverlayContents = Signal[Option[Node]]
+
 /** A cell that can have an overlay.
   */
-def CellWithOverlay(mainCellContents: Modifier[Div]*)(overlayContents: Signal[Option[Modifier[Div]]]) = {
+def CellWithOverlay(
+  mainCellContents: Modifier[Div]*
+)(overlayContents: CellOverlayContents): Div = {
   div(
     cls := "grid",
     div(
@@ -12,10 +17,10 @@ def CellWithOverlay(mainCellContents: Modifier[Div]*)(overlayContents: Signal[Op
       mainCellContents,
     ),
     // Laid out on top to take the same amount of space, so that when the update comes the layout would not change.
-    child.maybe <-- overlayContents.mapSome { mods =>
+    child.maybe <-- overlayContents.splitOption { (_, childSignal) =>
       div(
         cls := "col-start-1 row-start-1",
-        mods,
+        child <-- childSignal,
       )
     },
   )
