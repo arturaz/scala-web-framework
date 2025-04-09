afc1367077e0e64228a76bb13e58c27c61d4fc68
Artūras Šlajus <x11@arturaz.net>
2025-04-09 15:24:57 +0300

    Move extensions from `Seq` to `IterableOnce`

--- shared/src/framework/exts/IterableOnceExts.scala
+++ shared/src/framework/exts/IterableOnceExts.scala
@@ -4,4 +4,11 @@ import scala.collection.immutable.SortedSet
 
 extension [A](iter: IterableOnce[A]) {
   def toSortedSet(using Ordering[A]): SortedSet[A] = SortedSet.from(iter)
+
+  /** As [[IterableOnce.find]] but returns an [[Option]] of the value and its index. */
+  def findWithIndex(f: A => Boolean): Option[(A, Int)] =
+    iter.iterator.zipWithIndex.find { case (a, _) => f(a) }
+
+  def findMap[B](f: A => Option[B]): Option[B] =
+    iter.iterator.map(f).find(_.isDefined).flatten
 }
