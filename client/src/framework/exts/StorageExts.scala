bdafed7fdf15d8e6807ed99e865381116cdac790
Artūras Šlajus <x11@arturaz.net>
2025-04-15 13:10:37 +0300

    `Storage.keys` and `.all`

---
+++ client/src/framework/exts/StorageExts.scala
@@ -0,0 +1,14 @@
+package framework.exts
+
+import org.scalajs.dom.Storage
+
+extension (s: Storage) {
+
+  /** Returns all keys in the storage. */
+  def keys(): IndexedSeq[String] = 
+    (0 until s.length).map(s.key)
+
+  /** Returns all items in the storage. */
+  def all(): IndexedSeq[(String, String)] =
+    s.keys().map(key => (key, s.getItem(key)))
+}
