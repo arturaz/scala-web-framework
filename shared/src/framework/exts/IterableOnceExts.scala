a2d8a88a60f17df7c54c692bcac5124b3cbcb19f
Artūras Šlajus <x11@arturaz.net>
2025-04-22 21:49:18 +0300

    Change the encoding of extension methods.

--- shared/src/framework/exts/IterableOnceExts.scala
+++ shared/src/framework/exts/IterableOnceExts.scala
@@ -2,17 +2,21 @@ package framework.exts
 
 import scala.collection.immutable.SortedSet
 
-extension [A](iter: IterableOnce[A]) {
-  def toSortedSet(using Ordering[A]): SortedSet[A] = SortedSet.from(iter)
+trait IterableOnceExts {
+  extension [A](iter: IterableOnce[A]) {
+    def toSortedSet(using Ordering[A]): SortedSet[A] = SortedSet.from(iter)
 
-  /** As [[IterableOnce.find]] but returns an [[Option]] of the value and its index. */
-  def findWithIndex(f: A => Boolean): Option[(A, Int)] =
-    iter.iterator.zipWithIndex.find { case (a, _) => f(a) }
+    /** As [[IterableOnce.find]] but returns an [[Option]] of the value and its index. */
+    def findWithIndex(f: A => Boolean): Option[(A, Int)] =
+      iter.iterator.zipWithIndex.find { case (a, _) => f(a) }
 
-  def findMap[B](f: A => Option[B]): Option[B] =
-    iter.iterator.map(f).find(_.isDefined).flatten
+    def findMap[B](f: A => Option[B]): Option[B] =
+      iter.iterator.map(f).find(_.isDefined).flatten
 
-  /** Keeps only the [[Some]] values. */
-  def mapFilter[B](f: A => Option[B]): Iterator[B] =
-    iter.iterator.map(f).collect { case Some(b) => b }
+    /** Keeps only the [[Some]] values. */
+    def mapFilter[B](f: A => Option[B]): Iterator[B] =
+      iter.iterator.map(f).collect { case Some(b) => b }
+  }
 }
+
+given frameworkIterableOnceExts: IterableOnceExts = new IterableOnceExts {}
