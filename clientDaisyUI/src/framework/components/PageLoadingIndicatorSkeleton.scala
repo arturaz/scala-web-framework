3bd19173cd3bb014a611c75658d5406e13b47f2c
Artūras Šlajus <x11@arturaz.net>
2025-05-12 13:40:17 +0300

    WIP: page loading indicator skeleton reload button

--- clientDaisyUI/src/framework/components/PageLoadingIndicatorSkeleton.scala
+++ clientDaisyUI/src/framework/components/PageLoadingIndicatorSkeleton.scala
@@ -2,16 +2,34 @@ package framework.components
 
 import com.raquo.laminar.api.L.*
 import framework.utils.PageRenderResult
+import scala.concurrent.Future
+import scala.concurrent.duration.*
 
 object PageLoadingIndicatorSkeleton extends PageLoadingIndicator {
-  def html: HtmlElement =
-    div(
-      cls := "flex flex-col gap-4",
-      div(cls := "skeleton h-32 w-72"),
-      div(cls := "skeleton h-4 w-28"),
-      div(cls := "skeleton h-4 w-full"),
-      div(cls := "skeleton h-4 w-full"),
-    )
+  case class ReloadButton(
+    text: Signal[String],
+    appearAfter: FiniteDuration = 3.seconds,
+  )
 
-  def renderResult: PageRenderResult = PageRenderResult.fromElement(html)
+  override def html: HtmlElement = html(reloadButton = None).html
+
+  def html(reloadButton: Option[ReloadButton] = None): PageLoadingIndicator = new {
+    override def html =
+      div(
+        cls := "flex flex-col gap-4",
+        div(cls := "skeleton h-32 w-72"),
+        div(cls := "skeleton h-4 w-28"),
+        div(cls := "skeleton h-4 w-full"),
+        div(cls := "skeleton h-4 w-full"),
+        child.maybe <-- (reloadButton match {
+          case Some(reloadButton) =>
+            Signal.fromFuture(Future.delay(reloadButton.appearAfter)).mapSome { _ =>
+              // TODO: render a button.
+              div()
+            }
+
+          case None => Signal.fromValue(None)
+        }),
+      )
+  }
 }
