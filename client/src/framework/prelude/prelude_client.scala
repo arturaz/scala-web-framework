f1107c4c36776cc72ffbb17d4c20381085f2f67e
Artūras Šlajus <x11@arturaz.net>
2025-06-03 16:01:08 +0300

    the `js.native` is broken, you have to import it anyway

--- client/src/framework/prelude/prelude_client.scala
+++ client/src/framework/prelude/prelude_client.scala
@@ -63,7 +63,6 @@ object js {
 
   type UndefOr[A] = scalajs.js.UndefOr[A]
 
-  inline def native: Nothing = scalajs.js.native
   inline def undefined: UndefOr[Nothing] = scalajs.js.undefined
 
   inline def isUndefined(a: Any): Boolean = scalajs.js.isUndefined(a)
