0e82487cb16dd4c9e61901f11d9a59ed3b0f1bd4
Artūras Šlajus <x11@arturaz.net>
2025-09-18 14:19:08 +0300

    `RedisKey.map` and `.mapPrefix`

--- serverRedis/src/framework/redis/RedisKey.scala
+++ serverRedis/src/framework/redis/RedisKey.scala
@@ -1,9 +1,8 @@
 package framework.redis
 
-import cats.kernel.Semigroup
 import cats.syntax.semigroup.*
 import cats.syntax.show.*
-import cats.Show
+import cats.{Semigroup, Show}
 
 /** A prefixed Redis key. */
 final case class RedisKey[+Key: Semigroup](prefix: RedisKeyPrefix[Key], key: Key) {
@@ -15,4 +14,10 @@ object RedisKey {
   given [Key: Show]: Show[RedisKey[Key]] = _.fullKey.show
   given [Key]: Conversion[RedisKey[Key], Key] = _.fullKey
   given [Key: Show]: Conversion[RedisKey[Key], String] = _.fullKey.show
+
+  extension [Key](key: RedisKey[Key]) {
+    def mapPrefix(f: Key => Key)(using Semigroup[Key]): RedisKey[Key] = key.copy(prefix = key.prefix.map(f))
+
+    def map(f: Key => Key)(using Semigroup[Key]): RedisKey[Key] = key.copy(key = f(key.key))
+  }
 }
