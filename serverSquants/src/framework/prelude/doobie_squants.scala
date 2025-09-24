23504e9c6984f8f7839d8cbb53eea063ed436501
Artūras Šlajus <x11@arturaz.net>
2025-09-24 12:49:19 +0300

    `Meta[LengthIn[U]]`

--- serverSquants/src/framework/prelude/doobie_squants.scala
+++ serverSquants/src/framework/prelude/doobie_squants.scala
@@ -1,11 +1,15 @@
 package framework.prelude
 
+import doobie.util.meta.Meta
 import doobie.util.{Get, Put}
 import framework.data.LengthIn
 import squants.space.LengthUnit
 
-given [U <: LengthUnit](using unit: ValueOf[U]): Get[LengthIn[U]] =
+given getLengthIn[U <: LengthUnit](using unit: ValueOf[U]): Get[LengthIn[U]] =
   Get[Double].map(LengthIn.fromValue)
 
-given [U <: LengthUnit](using unit: ValueOf[U]): Put[LengthIn[U]] =
+given putLengthIn[U <: LengthUnit](using unit: ValueOf[U]): Put[LengthIn[U]] =
   Put[Double].contramap(_.valueIn(unit.value))
+
+given metaLengthIn[U <: LengthUnit](using unit: ValueOf[U]): Meta[LengthIn[U]] =
+  new Meta(getLengthIn, putLengthIn)
