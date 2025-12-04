09ade2b80fa3ed54623031ec7a25929cefceffd1
Artūras Šlajus <x11@arturaz.net>
2025-12-04 17:40:08 +0200

    `NewtypeString.urlConvertible`

--- shared/src/framework/utils/NewtypeString.scala
+++ shared/src/framework/utils/NewtypeString.scala
@@ -6,6 +6,7 @@ import io.scalaland.chimney.Transformer
 import framework.exts.*
 import framework.prelude.{*, given}
 import cats.kernel.Order
+import urldsl.errors.DummyError
 
 /** A newtype wrapping a [[String]]. */
 trait NewtypeString extends Newtype.Of[String] {
@@ -17,6 +18,9 @@ trait NewtypeString extends Newtype.Of[String] {
 
   given tapirCodec: TapirCodec[String, Type, TapirCodecFormat.TextPlain] =
     TapirCodec.string.mapEither(make.asString)(unwrap)
+
+  given urlConvertible: UrlConvertible[Type, DummyError] =
+    UrlConvertible.fromCodec
 
   given Transformer[Type, String] = unwrap
 
