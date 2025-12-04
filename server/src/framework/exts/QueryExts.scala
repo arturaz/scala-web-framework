8acf28d4e3178ecb0c89638910fbed831d786ada
Artūras Šlajus <x11@arturaz.net>
2025-12-04 20:39:24 +0200

    Yeah, this was a bad idea.

--- server/src/framework/exts/QueryExts.scala
+++
@@ -1,9 +0,0 @@
-package framework.exts
-
-import framework.db.*
-
-extension (query: Query0[Unit]) {
-
-  /** Returns whether there is at least one result. */
-  def single: ConnectionIO[Boolean] = query.option.map(_.isDefined)
-}
