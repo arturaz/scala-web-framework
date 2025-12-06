c8bc089d5e54ca0b2bed18f22dd0da8587f82a31
Artūras Šlajus <x11@arturaz.net>
2025-12-06 17:28:27 +0200

    jsoniter-scala-circe

--- shared/src/framework/prelude/circe.scala
+++ shared/src/framework/prelude/circe.scala
@@ -16,6 +16,9 @@ export framework.utils.CirceKeyCodec
 export io.circe.syntax.{EncoderOps, KeyOps}
 export io.circe.parser.{decode as decodeJson, decodeAccumulating as decodeJsonAccumulating, parse as parseJson}
 
+// Export faster jsoniter-based circe codecs to default scope
+export com.github.plokhotnyuk.jsoniter_scala.circe.CirceCodecs.*
+
 given CirceKeyEncoder[Uri] = CirceKeyEncoder.instance(_.toString)
 given CirceKeyDecoder[Uri] = CirceKeyDecoder.instance(Uri.parse(_).toOption)
 
