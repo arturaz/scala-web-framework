7616aedcd3fec14e716a7a76b537e9f4106b3613
Artūras Šlajus <x11@arturaz.net>
2025-12-06 23:37:33 +0200

    `PostgresqlConfig.SslMode`

--- server/src/framework/config/PostgresqlConfig.scala
+++ server/src/framework/config/PostgresqlConfig.scala
@@ -15,6 +15,7 @@ import fly4s.Fly4s
 import fly4s.data.Fly4sConfig
 import org.typelevel.otel4s.trace.{Tracer, TracerProvider}
 import fly4s.data.ValidatePattern
+import org.tpolecat.typename.TypeName
 
 case class PostgresqlConfig(
   username: String = "postgres",
@@ -22,9 +23,10 @@ case class PostgresqlConfig(
   database: String,
   host: Host = host"localhost",
   port: Port = port"5432",
+  sslMode: Option[PostgresqlConfig.SslMode] = None,
 ) {
   def jdbcUrl: String =
-    s"jdbc:postgresql://${host.show}:${port.show}/${database.show}"
+    s"jdbc:postgresql://${host.show}:${port.show}/${database.show}?${sslMode.fold("")(sslMode => s"sslmode=${sslMode.asString}")}"
 
   def defaultFly4sConfig: Fly4sConfig = Fly4sConfig(ignoreMigrationPatterns =
     List(
@@ -69,6 +71,20 @@ case class PostgresqlConfig(
     } yield TracedTransactor(hikari, logHandler.getOrElse(LogHandler.noop), tracerConfig)
 }
 object PostgresqlConfig {
+  enum SslMode(val asString: String) {
+    case Require extends SslMode("require")
+    case VerifyCa extends SslMode("verify-ca")
+    case VerifyFull extends SslMode("verify-full")
+  }
+  object SslMode {
+    given ConfigDecoder[String, SslMode] =
+      summon[ConfigDecoder[String, String]].mapOption(summon[TypeName[SslMode]].value) {
+        case SslMode.Require.asString    => Some(SslMode.Require)
+        case SslMode.VerifyCa.asString   => Some(SslMode.VerifyCa)
+        case SslMode.VerifyFull.asString => Some(SslMode.VerifyFull)
+        case _                           => None
+      }
+  }
 
   /** The class name of the JDBC driver. */
   final val JDBCDriverName: String = "org.postgresql.Driver"
@@ -83,5 +99,6 @@ object PostgresqlConfig {
     ciris.env(prefix("POSTGRESQL_DATABASE")).as[String],
     ciris.env(prefix("POSTGRESQL_HOST")).as[Host].default(host"localhost"),
     ciris.env(prefix("POSTGRESQL_PORT")).as[Port].default(port"5432"),
+    ciris.env(prefix("POSTGRESQL_SSL_MODE")).as[SslMode].option,
   ).parMapN(apply)
 }
