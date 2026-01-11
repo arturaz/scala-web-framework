93bc1ebe1cff47ef9b8b7a548f29a58aa1e04180
Artūras Šlajus <x11@arturaz.net>
2026-01-11 02:19:44 +0200

    `CirceCodec.fromTapirCodec`

--- shared/src/framework/exts/CirceCodecExts.scala
+++ shared/src/framework/exts/CirceCodecExts.scala
@@ -7,10 +7,21 @@ extension (obj: CirceCodec.type) {
   /** Provide an instance of [[CirceCodec]] for [[A]] if they implement [[CirceEncoder]] and [[CirceDecoder]]. */
   def fromUsing[A](using dec: CirceDecoder[A], enc: CirceEncoder[A]): CirceCodec[A] = CirceCodec.from(dec, enc)
 
-  /** Provice an instance of [[CirceCodec]] for [[A]] if they implement [[CirceKeyEncoder]] and [[CirceKeyDecoder]]. */
+  /** Provide an instance of [[CirceCodec]] for [[A]] if they implement [[CirceKeyEncoder]] and [[CirceKeyDecoder]]. */
   def fromKeyCodecs[A](using dec: CirceKeyDecoder[A], enc: CirceKeyEncoder[A]): CirceCodec[A] = {
     val jsonDec = summon[CirceDecoder[String]].emap(str => dec(str).toRight("Failed to parse string"))
     val jsonEnc = summon[CirceEncoder[String]].contramap(enc.apply)
     CirceCodec.from(jsonDec, jsonEnc)
   }
+
+  /** Provide an instance of [[CirceCodec]] for [[A]] if they implement [[TapirCodec]]. */
+  def fromTapirCodec[LowLevel, HighLevel](using
+    codec: TapirCodec[LowLevel, HighLevel, TapirCodecFormat.TextPlain],
+    circeDecoder: CirceDecoder[LowLevel],
+    circeEncoder: CirceEncoder[LowLevel],
+  ): CirceCodec[HighLevel] =
+    CirceCodec.from(
+      circeDecoder.emap(codec.decode(_).toEither.left.map(err => err.toString())),
+      circeEncoder.contramap(codec.encode),
+    )
 }
